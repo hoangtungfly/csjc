b838dc69ae0a4c1d687b23f19d64d27f2896a3c6
HOANGTUNG <tunghus1993@gmail.com>
2018-07-01 21:59:01 +0700

    upcode

--- application/aiem/views/main/block/block_contact.php
+++ application/aiem/views/main/block/block_contact.php
@@ -7,24 +7,31 @@ use common\models\settings\SystemSettingSearch;
 use yii\helpers\Html;
 
 $email = SystemSettingSearch::getValue('email');
+$config = $this->context->array_config();
 ?>
-<div class="block block-contact">
-    <div class="container">
-        <h1><?= $item['title'] ?></h1>
-        <div class="contact-description"><?= $item['description'] ?></div>
-        <div class="clear"></div>
-        <div class="row">
-            <div class="col-xs-12 col-sm-6 col-md-6">
-                <h2 class="title-3"><span>Trụ Sở Chính</span></h2>
+<div class="container company">
+    <h1 class="text-center"><?= $item['title'] ?></h1>
+    <h2 class="text-center"><?= $item['description'] ?></h2>
+    <hr>
+</div>
+
+<div class="container contact">
+    <div class="row_1">
+        <div class="col-xs-6">
+            <h4>TRỤ SỞ CHÍNH</h4>
+            <div class="content">
                 <p>Phòng 411 Toà nhà TOYOTA Mỹ Đình, Số 15 Phạm Hùng, Quận Nam Từ Liêm, Thành phố Hà Nội, Việt Nam.</p>
-                <p><span>Tel: +84-24 3 795 7717</span></p>
-                <?php if($email):?>
-                <p>Email: <?=$email?></p>
+                <p>Tel: +84-24 3 795 7717</p>
+                <?php if(isset($config['email']) && $config['email']):?>
+                <p>Email: <?=$config['email']?></p>
                 <?php endif;?>
                 <p>Hotline: (+84) 983.384.888</p>
             </div>
-            <div class="col-xs-12 col-sm-6 col-md-6">
-                <?php
+            <iframe height="450" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=hanoi&key=AIzaSyBD1yh4p30kaKpA2idu_e_rcR1GZkUYXCA" allowfullscreen></iframe>
+        </div>
+        
+        <div class="col-xs-6">
+            <?php
                 $model = new ContactSearch();
                 $form = GlobalActiveForm::begin([
                     'id' => 'contact-form',
@@ -36,9 +43,9 @@ $email = SystemSettingSearch::getValue('email');
                     'action' => $this->createUrl('/contact/proccess'),
                     'fieldConfig' => [
                         'template' => "{input}{error}",
-                        'options' => [
-                            'class' => 'input-box'
-                        ]
+//                        'options' => [
+//                            'class' => 'form-control'
+//                        ]
                     ],
                     'options' => [
                         'class' => 'contact-form'
@@ -46,26 +53,14 @@ $email = SystemSettingSearch::getValue('email');
                 ]);
                 echo Html::hiddenInput('lang', app()->language);
                 ?>
-                <div class="form-contact-left">
-                    <h2><?= SettingsMessageSearch::t('contact', 'chi_tiet_lien_lac', 'Chi tiết liên lạc của bạn') ?><span class="red">*</span></h2>
-                    <?= $form->field($model, 'name')->textInput(['placeholder' => SettingsMessageSearch::t('contact', 'name', 'Tên của bạn')]) ?>
-                    <?= $form->field($model, 'phone')->textInput(['placeholder' => SettingsMessageSearch::t('contact', 'phone', 'Điện thoại của bạn')]) ?>
-                    <?= $form->field($model, 'email')->textInput(['placeholder' => SettingsMessageSearch::t('contact', 'email', 'Email')]) ?>
-
-                </div>
-                <div class="form-contact-right">
-                    <h2><?= SettingsMessageSearch::t('contact', 'vui_long_mo_ta_cau_hoi', 'Vui lòng mô tả câu hỏi của bạn') ?><span class="red">*</span></h2>
-                    <?= $form->field($model, 'content')->textarea(['style' => 'height:410px']) ?>
-                </div>
-
-                <div class="form-contact-submit">
-                    <button class="btn btn-primary bnone"><?= SettingsMessageSearch::t('contact', 'button_submit', 'Gửi') ?></button>
-                    <button class="btn btn-default bnone"><?= SettingsMessageSearch::t('contact', 'button_cancel', 'Làm lại') ?></button>
-                </div>
-                <?php GlobalActiveForm::end(); ?>
-                <div class="clear"></div>
-            </div>
+                <h4>LIÊN HỆ</h4>
+                <?= $form->field($model, 'name')->textInput(['placeholder' => SettingsMessageSearch::t('contact', 'name', 'Tên của bạn')]) ?>
+                <?= $form->field($model, 'email')->textInput(['placeholder' => SettingsMessageSearch::t('contact', 'email', 'Email')]) ?>
+                <?= $form->field($model, 'phone')->textInput(['placeholder' => SettingsMessageSearch::t('contact', 'phone', 'Điện thoại của bạn')]) ?>
+                <?= $form->field($model, 'title')->textInput(['placeholder' => SettingsMessageSearch::t('contact', 'title', 'Tiêu đề')]) ?>
+                <?= $form->field($model, 'content')->textarea(['placeholder' => SettingsMessageSearch::t('contact', 'content', 'Nội dung')]) ?>
+                <button type="submit" class=""><?= SettingsMessageSearch::t('contact', 'button_submit', 'Gửi liên hệ') ?></button>
+             <?php GlobalActiveForm::end(); ?>
         </div>
-        
     </div>
 </div>
