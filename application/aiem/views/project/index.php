009e4de47955ae54d296ceb01ffbae36d255614c
HOANGTUNG <tunghus1993@gmail.com>
2018-07-03 08:17:47 +0700

    upcode

---
+++ application/aiem/views/project/index.php
@@ -0,0 +1,70 @@
+<?php 
+use common\models\admin\SettingsMessageSearch;
+?>
+<div class="container company">
+    <h1 class="text-center"><?= SettingsMessageSearch::t('project','project_list_title', 'PROJECTS')?></h1>
+    <h2 class="text-center"><?= SettingsMessageSearch::t('project','project_list_description', 'Showcase of our work for clients')?></h2>
+    <hr>
+</div>
+<!--<div class="container timeline">
+    <ul>
+        <li><a href="">All</a></li>
+        <li><a href="">2014</a></li>
+        <li><a href="">2015</a></li>
+        <li><a href="">2016</a></li>
+        <li><a href="">2017</a></li>
+        <li><a href="">Completed</a></li>
+    </ul>
+</div>-->
+<div class="container projectsList">
+    <div class="col-xs-4">
+        <div class="item">
+            <img class="img-responsive" src="assets/imgs/project_list/project.jpg">
+            <div class="content">
+                <h4>Project title</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                <a class="general" href="#">More detail ></a>
+            </div>
+        </div>
+    </div>
+    <div class="col-xs-4">
+        <div class="item">
+            <img class="img-responsive" src="assets/imgs/project_list/project.jpg">
+            <div class="content">
+                <h4>Project title</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                <a class="general" href="#">More detail ></a>
+            </div>
+        </div>
+    </div>
+    <div class="col-xs-4">
+        <div class="item">
+            <img class="img-responsive" src="assets/imgs/project_list/project.jpg">
+            <div class="content">
+                <h4>Project title</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                <a class="general" href="#">More detail ></a>
+            </div>
+        </div>
+    </div>
+    <div class="col-xs-4">
+        <div class="item">
+            <img class="img-responsive" src="assets/imgs/project_list/project.jpg">
+            <div class="content">
+                <h4>Project title</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                <a class="general" href="#">More detail ></a>
+            </div>
+        </div>
+    </div>
+    <div class="col-xs-4">
+        <div class="item">
+            <img class="img-responsive" src="assets/imgs/project_list/project.jpg">
+            <div class="content">
+                <h4>Project title</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                <a class="general" href="#">More detail ></a>
+            </div>
+        </div>
+    </div>
+</div>
