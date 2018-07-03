41be670e3884eea0fdafb7928e32d5f852eabba3
FIVE9\tunglx <tunglx@five9.vn>
2018-07-03 09:53:34 +0700

    upcode

--- application/aiem/views/main/block/block_textright.php
+++ application/aiem/views/main/block/block_textright.php
@@ -1,10 +1,10 @@
 <div class="row row_<?=$key-1?>">
-    <div class="col-xs-6 col col_1 hidden-xxs" style="background: #0079AF url(<?= $item['image'] ?>);background-position: center;background-repeat: no-repeat;background-size: cover;"></div>
+    <div class="col-xs-6 col col_1 hidden-xxs" style="background: #0079AF url(<?= isset($item['image']) ? $item['image'] : ''?>);background-position: center;background-repeat: no-repeat;background-size: cover;"></div>
     <div class="col-xs-6 col col_2 full-expand-xxs">
         <div class="title">
-            <h4><?=$item['title']?></h4>
+            <h4><?=isset($item['title']) ? $item['title'] : ''?></h4>
         </div>
-        <p class="wow slideInUp" data-wow-duration="1.5s"><?= $item['description'] ?></p>
-        <button><?=$item['text2']?></button>
+        <p class="wow slideInUp" data-wow-duration="1.5s"><?= isset($item['description'])? $item['description'] : ''?></p>
+        <button><?=isset($item['text2']) ? $item['text2'] : ''?></button>
     </div>
 </div>
