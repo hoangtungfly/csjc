e3acfb268f139d3eb79eb30779fdaa21bc841dc4
FIVE9\tunglx <tunglx@five9.vn>
2018-07-04 09:13:03 +0700

    upcode

--- common/models/admin/SettingsVideoSearch.php
+++ common/models/admin/SettingsVideoSearch.php
@@ -23,32 +23,22 @@ class SettingsVideoSearch extends SettingsVideo {
         $dataProvider = new ActiveDataProvider([
             'query' => $query,
         ]);
+        
 
-//        $query->andFilterWhere([
-//            'table_id' => $this->table_id,
-//            'created_time' => $this->created_time,
-//            'modified_time' => $this->modified_time,
-//            'created_by' => $this->created_by,
-//            'modified_by' => $this->modified_by,
-//            'status' => $this->status,
-//            'checkview' => $this->checkview,
-//            'checksearch' => $this->checksearch,
-//            'beginimport' => $this->beginimport,
-//            'columncheck' => $this->columncheck,
-//            'columnaction' => $this->columnaction,
-//            'columnid' => $this->columnid,
-//        ]);
-//
-//        $query->andFilterWhere(['like', 'name', $this->name])
-//                ->andFilterWhere(['like', 'table_name', $this->table_name])
-//                ->andFilterWhere(['like', 'condition', $this->condition])
-//                ->andFilterWhere(['like', 'orderby', $this->orderby])
-//                ->andFilterWhere(['like', 'attrsearch', $this->attrsearch])
-//                ->andFilterWhere(['like', 'attrarange', $this->attrarange])
-//                ->andFilterWhere(['like', 'attrchoice', $this->attrchoice])
-//                ->andFilterWhere(['like', 'join', $this->join])
-//                ->andFilterWhere(['like', 'excel', $this->excel]);
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'type' => $this->type,
+            'created_by' => $this->created_by,
+            'created_time' => $this->created_time,
+            'modified_by' => $this->modified_by,
+            'modified_time' => $this->modified_time,
+            'status' => $this->status,
+        ]);
 
+        $query->andFilterWhere(['like', 'name', $this->name])
+                ->andFilterWhere(['like', 'table_name', $this->table_name])
+                ->andFilterWhere(['like', 'link', $this->link])
+                ->andFilterWhere(['like', 'base_url', $this->base_url]);
         return $dataProvider;
     }
 }
--- common/models/contact/ContactSearch.php
+++ common/models/contact/ContactSearch.php
@@ -24,9 +24,9 @@ class ContactSearch extends Contact {
         $result = parent::rules();
 //        $result[] = [['captcha'], 'captcha', 'on' => 'frontend','message' => SettingsMessageSearch::t('form','captcha_captcha','Mã bảo mật không chính xác.')];
 //        $result[] = [['captcha'], 'required', 'on' => 'frontend','message' => SettingsMessageSearch::t('form','captcha_required','Mã bảo mật không được để rỗng.')];
-        $result[] = [['phone','email'], 'required', 'on' => 'frontend','message' => SettingsMessageSearch::t('form','required','{attribute} không được để rỗng.')];
-        $result[] = [['email'], 'email', 'on' => 'frontend','message' => SettingsMessageSearch::t('form','email_required','Email không chính xác.')];
-        $result[] = [['phone'], 'match', 'pattern' => '/^([0-9]{2,4}|\([0-9]{2,4}\))(| )[0-9]{5,18}$/', 'message' => SettingsMessageSearch::t('contact', 'validate_phone','Please enter correct contact phone'), 'on' => 'frontend'];
+//        $result[] = [['phone','email'], 'required', 'on' => 'frontend','message' => SettingsMessageSearch::t('form','required','{attribute} không được để rỗng.')];
+//        $result[] = [['email'], 'email', 'on' => 'frontend','message' => SettingsMessageSearch::t('form','email_required','Email không chính xác.')];
+//        $result[] = [['phone'], 'match', 'pattern' => '/^([0-9]{2,4}|\([0-9]{2,4}\))(| )[0-9]{5,18}$/', 'message' => SettingsMessageSearch::t('contact', 'validate_phone','Please enter correct contact phone'), 'on' => 'frontend'];
         return $result;
     }
 }
